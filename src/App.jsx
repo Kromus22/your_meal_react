b2c5582c0f6f490c4f81ba6393bc56cabc0e0c11
Kromus22 <kromus2@gmail.com>
2023-02-21 15:13:37 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Catalog } from "./components/Catalog/Catalog";
+import { Container } from "./components/Container/Container";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
+import { Navigation } from "./components/Navigation/Navigation";
+
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Navigation />
+        <Catalog />
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+
