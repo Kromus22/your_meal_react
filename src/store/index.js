31ec31cd38856a4df26adadfa2612c20d4d5e857
Kromus22 <kromus2@gmail.com>
2023-02-28 19:00:01 +0500

    +modal

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
-import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import categoryReducer from "./category/categorySlice.js";
 import productReducer from "./product/productSlice.js";
 import orderReducer, { localStorageMiddleware } from "./order/orderSlice.js";
+import modalReducer from "./modalDelivery/modalDeliverySlice.js";
 
 
 export const store = configureStore({
@@ -9,6 +10,7 @@ export const store = configureStore({
     category: categoryReducer,
     product: productReducer,
     order: orderReducer,
+    modal: modalReducer,
   },
 
   middleware: getDefaultMiddleware =>
